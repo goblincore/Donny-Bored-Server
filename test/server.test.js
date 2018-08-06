e4603355823ab4016e461ce35f9df299e334a391
Jonny Cheng <jcheng02@mica.edu>
2018-08-06 16:31:43 -0400

    added basic test, see if it works, it does

--- test/server.test.js
+++ test/server.test.js
@@ -1,76 +1,77 @@
 'use strict';
-
-// Clear the console before each run
-// process.stdout.write("\x1Bc\n");
-const {TEST_DATABASE_URL} = require('../config');
 const chai = require('chai');
-const chaiHttp = require('chai-http');
-
 const app = require('../server');
-const {dbConnect, dbDisconnect} = require('../db-knex');
+const chaiHttp = require('chai-http');
+const {TEST_DATABASE_URL} = require('../config');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-before(function() {
-  return dbConnect(TEST_DATABASE_URL);
-});
-  
-after(function() {
-  return dbDisconnect();
-});
-  
-describe('Mocha and Chai', function() {
-  it('should be properly setup', function() {
+// Clear the console before each run
+process.stdout.write('\x1Bc\n');
+
+const createKnex = require('knex');
+
+const {DATABASE_URL} = require('../config');
+
+let knex = null;
+
+function dbConnect(url = DATABASE_URL) {
+  knex = createKnex({
+    client: 'pg',
+    connection: url
+  });
+}
+
+function dbDisconnect() {
+  return knex.destroy();
+}
+
+function dbGet() {
+  return knex;
+}
+
+module.exports = {
+  dbConnect,
+  dbDisconnect,
+  dbGet
+};
+
+describe('Reality check', function () {
+
+  it('true should be true', function () {
     expect(true).to.be.true;
   });
-});
 
-describe('Reality Check', () => {
-
-  it('true should be true', () => {
-    expect(true).to.be.true;
-  });
-
-  it('2 + 2 should equal 4', () => {
+  it('2 + 2 should equal 4', function () {
     expect(2 + 2).to.equal(4);
   });
 
 });
 
-describe('Environment', () => {
+describe('404 handler', function () {
 
-  it('NODE_ENV should be "test"', () => {
-    expect(process.env.NODE_ENV).to.equal('test');
+  it('should respond with 404 when given a bad path', function () {
+    return chai.request(app)
+      .get('/DOES/NOT/EXIST')
+      .then(res => {
+        expect(res).to.have.status(404);
+      });
   });
-
+  
 });
 
-describe('Basic Express setup', () => {
-
-  describe('Express static', () => {
-
-    it('GET request "/" should return the index page', () => {
-      return chai.request(app)
-        .get('/')
-        .then(function (res) {
-          expect(res).to.exist;
-          expect(res).to.have.status(200);
-          expect(res).to.be.html;
-        });
+describe('Mocha and Chai', function() {
+    it('should be properly setup', function() {
+      expect(true).to.be.true;
     });
-
   });
 
-  describe('404 handler', () => {
 
-    it('should respond with 404 when given a bad path', () => {
-      return chai.request(app)
-        .get('/bad/path')
-        .then(res => {
-          expect(res).to.have.status(404);
-        });
-    });
-
+  before(function() {
+    return dbConnect(TEST_DATABASE_URL);
   });
-});
+  
+  after(function() {
+    return dbDisconnect();
+  });
